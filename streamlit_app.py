44d567af67fccf21c1134716f5b847fa2af0a3a7
Turkmen1Mehmet <52583698+Turkmen1Mehmet@users.noreply.github.com>
2024-04-19 00:10:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 from difrfm import create_rfm
-from datasetOrbit import datasetOrbit
+from datasetOrbit2 import datasetOrbit
 from datetime import time
 import base64
 import random
